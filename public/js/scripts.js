c45492cd3e773d7b36c1f49fcffea8944fd94d9b
andrefelix <afelixfreitas@gmail.com>
2018-06-30 07:43:43 -0300

    Added function for manipulate the image delete process

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -16,4 +16,26 @@ $(function() {
 			$('.likes-count').text(data.likes);
 		});
 	});
+
+	$('#btn-delete').on('click', function(event) {
+		event.preventDefault();
+
+		const response = confirm('You are sure that delete this image?');
+
+		if (response) {
+			const $this = $(this);
+			const imgId = $(this).data('id');
+
+			$.ajax({
+				url: '/images/' + imgId,
+				type: 'DELETE'
+			}).done(function(deleted) {
+				if (deleted) {
+					$this.removeClass('btn-danger').addClass('btn-success');
+					$this.find('i').removeClass('fa-times').addClass('fa-checked');
+					$this.append('<span> Deleted!</span>');
+				}
+			});
+		}
+	});
 });
